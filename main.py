fa0dca5b2a9d8239bbdc2c39e06ef47bf6f49030
JoseGermanx <jgermanmm@gmail.com>
2023-10-12 19:52:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("¿Cual es tu nombre?")
+nombre = input ()
+print("¿A qué te dedicas?")
+trabajo = input()
+print("primer objetivo")
+primer_objetivo = input()
+print("segundo objetivo")
+segundo_objetivo = input()
+print("primera comida")
+primera_comida = input()
+print("segunda comida")
+segunda_comida = input()
+print("sentimiento")
+sentimiento = input()
+
+
+
+print(nombre + " started their first Generation course today. They are training as a " + trabajo + ". They found their cohort to be very " + primer_objetivo + " but their teacher was, at least, " + segundo_objetivo + ". For lunch they have " + primera_comida + " and " + segunda_comida + " while reviewing their notes. They feel " + sentimiento + " but are determined to complete the course.")
